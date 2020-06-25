40f0f19d6fcaea6042c15aa66ab0cd62c48e06b0
Aditya Singh <adity.347317@gmail.com>
2020-06-25 23:18:38 +0530

    Setting up our React app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav,Navbar,NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar expand="lg">
+      <Navbar.Brand href="/">Todo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Link to="/">Home</Link></Nav.Item>
+        </Nav>
+        <Nav pullRight>
+            <Nav.Item><Link to="/signup">Signup</Link></Nav.Item>
+            <Nav.Item><Link to="/login">Login</Link></Nav.Item>
+          </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <Routes />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-znwbmi85f5it"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://jc338drd03.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_OvHI6MAcf",
+      APP_CLIENT_ID: "2t98g9clt80jkfrbm530rj36vh",
+      IDENTITY_POOL_ID: "us-east-2:77c39b46-4f9d-4d66-988f-ae39a513647e"
+    }
+  };
